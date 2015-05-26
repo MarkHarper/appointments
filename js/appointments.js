76d1f131f900f8ea4cd7e1ce3b78f6f1ac242a7c
Mark Harper <markharper20@gmail.com>
2015-05-26 16:40:04 -0400

    show/hide appt details page

--- js/appointments.js
+++ js/appointments.js
@@ -2,3 +2,15 @@ $(".newapp-btn").click(function() {
   $(".newappt-container").show();
   $(".appointments").hide();
 });
+
+$("li").click(function() {
+  $(".newappt-container").hide();
+  $(".appointments").hide();
+  $(".app-details").show();
+});
+
+$(".savebtn").click(function() {
+  $(".newappt-container").hide();
+  $(".appointments").hide();
+  $(".app-details").show();
+});
