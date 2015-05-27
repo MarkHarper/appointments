1da73e5e4c6b0d5e7be03415c08ed1e71f1c5ef1
Mark Harper <markharper20@gmail.com>
2015-05-27 15:03:03 -0400

    basic rendering

--- js/appointments.js
+++ js/appointments.js
@@ -1,28 +1,12 @@
-$(".newapp-btn").click(function() {
-  $(".newappt-container").show();
-  $(".appointments").hide();
+var views = {};
+
+$('script[type="text/html"]').each(function () {
+  var script = $(this);
+  views[script.attr('id')] = _.template(script.html(), { variable: 'm' });
+  script.remove();
 });
 
-$("li").click(function() {
-  $(".newappt-container").hide();
-  $(".appointments").hide();
-  $(".app-details").show();
-});
 
-$(".savebtn").click(function() {
-  $(".newappt-container").hide();
-  $(".appointments").hide();
-  $(".app-details").show();
-});
-
-$(".back").click(function() {
-  $(".newappt-container").hide();
-  $(".appointments").show();
-  $(".app-details").hide();
-});
-
-$(".edit-app").click(function() {
-  $(".newappt-container").show();
-  $(".appointments").hide();
-  $(".app-details").hide();
-});
+$('#main').html(views.appointment());
+//$('#main').html(views.newappointment());
+//$('#main').html(views.appointmentdetails());
