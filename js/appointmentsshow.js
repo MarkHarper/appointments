1e2d8536ee7546759cd4b4d9477dcb2dea020823
Mark Harper <markharper20@gmail.com>
2015-05-28 17:11:21 -0400

    styling changes/remove function

--- js/appointmentsshow.js
+++ js/appointmentsshow.js
@@ -1,4 +1,8 @@
 app.router.route('appointments', function () {
   app.show('appointment', { apps : app.apps } );
   //$('#main').html(app.appointment());
+  $('.delete-btn').on('click', function () {
+    //app.apps.remove['title'];
+    //document.location='#appointments';
+  })
 });
