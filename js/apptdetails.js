8b22e53f8b91271054b4402ef9b266f5b694ae60
Mark Harper <markharper20@gmail.com>
2015-05-31 23:30:47 -0400

    weather

--- js/apptdetails.js
+++ js/apptdetails.js
@@ -3,6 +3,9 @@ app.router.route('details/:title', function (title) {
   console.log(appt);
   app.show('appointmentdetails', appt);
   // $('#main').html(app.appointmentdetails());
+
+  getWeather();
+
   $('.delete-field').on('click', function (e) {
     e.preventDefault();
     if (confirm("Are you sure you want to delete this appointment?")) {
