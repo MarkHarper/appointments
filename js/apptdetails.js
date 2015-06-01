f5fb58cc3416b375900667ccb114bfd699e25b56
Mark Harper <markharper20@gmail.com>
2015-05-31 23:25:39 -0400

    weather

--- js/apptdetails.js
+++ js/apptdetails.js
@@ -2,6 +2,9 @@ app.router.route('details/:title', function (title) {
   var appt = app.apps.getByTitle(title);
   console.log(appt);
   app.show('appointmentdetails', appt);
+
+  getWeather();
+
   // $('#main').html(app.appointmentdetails());
   $('.delete-field').on('click', function (e) {
     e.preventDefault();
